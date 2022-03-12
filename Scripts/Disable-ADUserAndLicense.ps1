d8639effdc1b6ed6413ed1cf6d2d81adbc29a428
robbiepryan <88213739+robbiepryan@users.noreply.github.com>
2022-03-11 21:58:33 -0500

    Add logging feature

--- Scripts/Disable-ADUserAndLicense.ps1
+++ Scripts/Disable-ADUserAndLicense.ps1
@@ -1,32 +1,61 @@
+# # # # # # #
+# Variables #
+# # # # # # # 
+
+$user = ''
+$result = ''
+$correctUser = ''
+$upn = ''
+
+# # # # # # #
+# Get Input #
+# # # # # # #
+
 $user = Read-Host -Prompt "Enter username`n"
+
+# # # # # # #
+# Find User #
+# # # # # # #
 
 $result = (Get-aduser -Filter * |
 Where-Object {($_.SamAccountName -like "*$user*") -or ($_.Name -like "*$user*")}
 )
 
+# # # # # # # #
+# Verify User #
+# # # # # # # #
+
 Write-Host "`n$($result.Name) is the selected user."
 
 $correctUser = Read-Host -Prompt "`nIs this correct? (Y/N)`n"
 
-if ($correctUser -eq "Y") {
-    $upn = ($result).UserPrincipalName
+# # # # # #
+# Action! #
+# # # # # #
 
-    Disable-ADAccount -Identity $result.DistinguishedName
+switch ($correctUser) {
 
-    Connect-MsolService
+    "Y" {
+        Write-Host "Deactivating User AD Account..."
 
-    (get-MsolUser -UserPrincipalName $upn).licenses.AccountSkuId |
-        ForEach-Object{
-            Set-MsolUserLicense -UserPrincipalName $upn -RemoveLicenses $_
-}
+        $upn = ($result).UserPrincipalName
 
-if ($? -eq $true) {
-    Write-Host "Command Complete Successfully"
-} else {
-    Write-Host "An error occurred. Double check settings."
-}
+        Disable-ADAccount -Identity $result.DistinguishedName
 
-} else {
-    Write-Host "`nNo changes were made. Exiting...`n"
-    exit
+        Write-Host "Connecting to MsolService..."
+
+        Connect-MsolService
+
+        Write-Host "Removing Licenses..."
+        
+        (get-MsolUser -UserPrincipalName $upn).licenses.AccountSkuId |
+            ForEach-Object{
+                Set-MsolUserLicense -UserPrincipalName $upn -RemoveLicenses $_
+            }
+    } 
+
+    Default {
+        Write-Host "`nNo changes were made. Exiting...`n"
+        exit
+    }
 }
