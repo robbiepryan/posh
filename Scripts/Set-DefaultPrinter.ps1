b19122f515caff4748a97a494c9bf000fb9bb17c
robbiepryan <robbiepryan@gmail.com>
2022-03-30 21:21:09 -0400

    Refactored

--- Scripts/Set-DefaultPrinter.ps1
+++ Scripts/Set-DefaultPrinter.ps1
@@ -1,10 +1,20 @@
-$path = "~\DefaultPrinter.config"
+# Set config file location
+$ConfigFile = "~\DefaultPrinter.config"
 
-if ((Test-Path $path) -eq $false) {
-    $user_input = Read-Host -Prompt "Enter name of printer to set as default"
+<#
+ # Test if $ConfigFile exists. If $ConfigFile already exits, set default printer
+ # to the printer specified in the $ConfigFile. If $ConfigFile doesn't exist, ask
+ # user to enter the name of the printer to set as default and save that value as
+ # $UserInput. Then, do a wildcard search for installed printers that match
+ # $UserInput, and set that value equal to $ConfigFile
+ #> 
+
+if ((Test-Path $ConfigFile) -eq $false) {
+    $UserInput = Read-Host -Prompt "Enter name of printer to set as default"
     (Get-Printer |
-        Where-Object Name -like "*$user_input*").Name |
-        Out-File $path    
+        Where-Object Name -like "*$UserInput*").Name |
+        Out-File $ConfigFile
 } 
 
-(New-Object -ComObject WScript.Network).SetDefaultPrinter((Get-Content $path))
+# Set default printer to the printer name listed in $ConfigFile
+(New-Object -ComObject WScript.Network).SetDefaultPrinter((Get-Content $ConfigFile))
