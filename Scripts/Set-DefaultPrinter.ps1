0b5bc0c756445cee1d6dc129fd122ec77b50afd2
robbiepryan <robbiepryan@gmail.com>
2022-03-30 21:34:08 -0400

    Fixed comments

--- Scripts/Set-DefaultPrinter.ps1
+++ Scripts/Set-DefaultPrinter.ps1
@@ -2,11 +2,11 @@
 $ConfigFile = "~\DefaultPrinter.config"
 
 <#
- # Test if $ConfigFile exists. If $ConfigFile already exits, set default printer
- # to the printer specified in the $ConfigFile. If $ConfigFile doesn't exist, ask
- # user to enter the name of the printer to set as default and save that value as
- # $UserInput. Then, do a wildcard search for the first installed printer that
- # matches $UserInput, and write that value to $ConfigFile
+ # Test if the $ConfigFile exists. If the $ConfigFile already exits, set default
+ # printer to the printer specified in the $ConfigFile. If the $ConfigFile doesn't
+ # exist, ask user to enter the name of the printer to set as default and save the
+ # first result as $UserInput. Then, do a wildcard search for the first installed
+ # printer that matches $UserInput, and write that value to the $ConfigFile.
  #> 
 
 if ((Test-Path $ConfigFile) -eq $false) {
