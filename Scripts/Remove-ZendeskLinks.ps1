394446007c16498ee88374ab399923fbc8edf0ed
roberto-ryan <robert.ryan@virtechsystems.com>
2022-08-03 11:41:25 -0400

    Updated confirmation output

--- Scripts/Remove-ZendeskLinks.ps1
+++ Scripts/Remove-ZendeskLinks.ps1
@@ -9,6 +9,9 @@ foreach ($link in $supportLinks) {
     if ( (Get-Content $link 2>$null) -like "*https://virtechsystems.zendesk.com*" ) {
         #Write-Host "$link is a zendesk link" -ForegroundColor Red
         Remove-Item $link -Force
+        if ($?){
+            Write-Output "$link removed successfully."
+        }
     }
     else {
         #Write-Host "$link is NOT a zendesk link" -ForegroundColor Green
