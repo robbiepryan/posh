1e63926246cd9506c045c3742d055febbe33ccca
robbiepryan <robbiepryan@gmail.com>
2022-08-04 00:44:08 -0400

    initial commit

---
+++ Scripts/Search-VTSEventLog.ps1
@@ -0,0 +1,36 @@
+function Search-VTSEventLog {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position=0,Mandatory,
+            ParameterSetName = 'SearchTerm')]
+        [string]$SearchTerm
+        )
+        [array]$Logname = @(
+            "System"
+            "Application"
+            #"Security"
+        )
+        
+    $result = @()
+
+    foreach ($log in $Logname) {
+        Get-EventLog -LogName $log -EntryType Error, Warning -Newest 1 2>$null |
+        Where-Object Message -like "*$SearchTerm*" |
+        Select-Object TimeGenerated, Message |
+        ForEach-Object {
+            $result += [PSCustomObject]@{
+                TimeGenerated = $_.TimeGenerated
+                Message       = $_.Message
+                Log           = $log
+            }
+        }
+    }
+    
+    foreach ($log in $Logname) {
+        if ($null -eq ($result | Where-Object Log -like "$log")) {
+            Write-Host "$($log) Log - No Matches Found" -ForegroundColor Yellow
+        }
+    }
+
+    $result | Format-List
+}
