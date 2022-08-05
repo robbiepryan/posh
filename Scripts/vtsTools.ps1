dd37c15625e5e98495965fe43123a3ed4cc80c97
roberto-ryan <robert.ryan@virtechsystems.com>
2022-08-05 11:55:18 -0400

    Added Random Password Generator

--- Scripts/vtsTools.ps1
+++ Scripts/vtsTools.ps1
@@ -160,3 +160,18 @@ function rping {
         Write-Host "logfile saved to $output"
     }
 }
+
+function New-vtsRandomPassword {
+    $numbers = 0..9
+    $symbols = '!', '@', '#', '$', '%', '*', '?', '+', '='
+    $string = ( -join ((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) |
+            Get-Random -Count 12  |
+            ForEach-Object { [char]$_ }))
+    $number = $numbers | Get-Random
+    $symbol = $symbols | Get-Random
+    $NewPW = $string + $number + $symbol
+
+    $NewPW | Set-Clipboard
+
+    Write-Output "Random Password Copied to Clipboard"
+}
