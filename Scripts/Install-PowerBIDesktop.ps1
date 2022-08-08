cf1bd3ad3ae29f813b2e3883ea5d630050e9eff4
roberto-ryan <robert.ryan@virtechsystems.com>
2022-08-08 14:31:26 -0400

    Initial commit

---
+++ Scripts/Install-PowerBIDesktop.ps1
@@ -0,0 +1,26 @@
+try {
+    $link = 'https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe'
+    $downloadPath = "C:\temp"
+    $downloadName = (($link).Split("/"))[-1]
+  
+    # Create $downloadPath if it doesn't already exist
+    if (-not (Test-Path $downloadPath)) {
+        New-Item -Path $downloadPath -ItemType Directory
+    }
+    
+    # Download Installer
+    Invoke-WebRequest -Uri $link -UseBasicParsing -OutFile "$downloadPath\$downloadName"
+
+    # Get original UAC settings
+    $UAC = (Get-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin).ConsentPromptBehaviorAdmin
+
+    # Disable UAC to complete the uninstall
+    Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
+
+    # Start Silent Install
+    & $downloadPath\$downloadName -s -norestart ACCEPT_EULA=1
+}
+finally {
+    # Renable UAC
+    Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value $UAC
+}
