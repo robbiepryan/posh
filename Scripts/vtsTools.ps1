c94e8ff33136db5ca9de45ef2de7bdce39e35780
roberto-ryan <robert.ryan@virtechsystems.com>
2022-08-08 09:23:40 -0400

    Added function Get-vtsDisplayConnectionType

--- Scripts/vtsTools.ps1
+++ Scripts/vtsTools.ps1
@@ -243,26 +243,71 @@ function Out-vtsPhoneticAlphabet {
     $chars = ($InputObject).ToCharArray()
 
     foreach ($char in $chars) {
-        switch -Regex -CaseSensitive ($char)
-        {
-            '\d'
-            {
+        switch -Regex -CaseSensitive ($char) {
+            '\d' {
                 $result += ($nato["$char"])
                 break
             }
-            '[a-z]'
-            {
+            '[a-z]' {
                 $result += ($nato["$char"]).ToLower()
                 break
             }
-            '[A-Z]'
-            {
+            '[A-Z]' {
                 $result += ($nato["$char"]).ToUpper()
                 break
             }
 
-            Default {$result += $char}
+            Default { $result += $char }
         }
     }
     $result
+}
+
+function Get-vtsDisplayConnectionType {
+    $adapterTypes = @{
+        '-2'         = 'Unknown'
+        '-1'         = 'Unknown'
+        '0'          = 'VGA'
+        '1'          = 'S-Video'
+        '2'          = 'Composite'
+        '3'          = 'Component'
+        '4'          = 'DVI'
+        '5'          = 'HDMI'
+        '6'          = 'LVDS'
+        '8'          = 'D-Jpn'
+        '9'          = 'SDI'
+        '10'         = 'DisplayPort (external)'
+        '11'         = 'DisplayPort (internal)'
+        '12'         = 'Unified Display Interface'
+        '13'         = 'Unified Display Interface (embedded)'
+        '14'         = 'SDTV dongle'
+        '15'         = 'Miracast'
+        '16'         = 'Internal'
+        '2147483648' = 'Internal'
+    }
+    $arrMonitors = @()
+    $monitors = Get-WmiObject WmiMonitorID -Namespace root/wmi
+    $connections = Get-WmiObject WmiMonitorConnectionParams -Namespace root/wmi
+    foreach ($monitor in $monitors) {
+        $manufacturer = $monitor.ManufacturerName
+        $name = $monitor.UserFriendlyName
+        $connectionType = ($connections | Where-Object { $_.InstanceName -eq $monitor.InstanceName }).VideoOutputTechnology
+        if ($manufacturer -ne $null) { $manufacturer = [System.Text.Encoding]::ASCII.GetString($manufacturer -ne 0) }
+        if ($name -ne $null) { $name = [System.Text.Encoding]::ASCII.GetString($name -ne 0) }
+        $connectionType = $adapterTypes."$connectionType"
+        if ($connectionType -eq $null) { $connectionType = 'Unknown' }
+        if (($manufacturer -ne $null) -or ($name -ne $null)) { $arrMonitors += "$manufacturer $name ($connectionType)" }
+    }
+    $i = 0
+    $strMonitors = ''
+    if ($arrMonitors.Count -gt 0) {
+        foreach ($monitor in $arrMonitors) {
+            if ($i -eq 0) { $strMonitors += $arrMonitors[$i] }
+            else { $strMonitors += "`n"; $strMonitors += $arrMonitors[$i] }
+            $i++
+        }
+    }
+    if ($strMonitors -eq '') { $strMonitors = 'None Found' }
+    $strMonitors
+    
 }
