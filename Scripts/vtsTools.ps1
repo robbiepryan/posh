b9dd8b116826db4093b4ba1c5d3f22e986409748
roberto-ryan <robert.ryan@virtechsystems.com>
2022-08-08 09:21:43 -0400

    Added support for symbols to Out-PhoneticAlphabet

--- Scripts/vtsTools.ps1
+++ Scripts/vtsTools.ps1
@@ -184,127 +184,85 @@ function New-vtsRandomPassword {
     Write-Output "Random Password Copied to Clipboard"
 }
 
-#FROM https://www.powershellgallery.com/packages/Out-PhoneticAlphabet/0.2.0/Content/Out-PhoneticAlphabet.ps1
-function Out-PhoneticAlphabet
-{
-    [CmdletBinding(SupportsShouldProcess=$true,
-                  ConfirmImpact='Low')]
+function Out-vtsPhoneticAlphabet {
+    [CmdletBinding()]
     [OutputType([String])]
     Param
     (
         # Input string to convert
-        [Parameter(Mandatory=$true, 
-                   ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$false,
-                   ValueFromRemainingArguments=$false,
-                   Position=0)]
+        [Parameter(Mandatory = $true, 
+            ValueFromPipeline = $true,
+            Position = 0)]
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
-        [ValidatePattern("^[0-9a-zA-Z\.\-]+$")]
         [string[]]
         $InputObject
     )
-    Begin
-    {
-        Write-Verbose -Message 'Listing Parameters utilized:'
-        $PSBoundParameters.GetEnumerator() | ForEach-Object { Write-Verbose -Message "$($PSItem)" }
+    $result = @()
+    $nato = @{
+        '0' = '(ZERO)'
+        '1' = '(ONE)'
+        '2' = '(TWO)'
+        '3' = '(THREE)'
+        '4' = '(FOUR)'
+        '5' = '(FIVE)'
+        '6' = '(SIX)'
+        '7' = '(SEVEN)'
+        '8' = '(EIGHT)'
+        '9' = '(NINE)'
+        'a' = 'alfa'
+        'b' = 'bravo'
+        'c' = 'charlie'
+        'd' = 'delta'
+        'e' = 'echo'
+        'f' = 'foxtrot'
+        'g' = 'golf'
+        'h' = 'hotel'
+        'i' = 'india'
+        'j' = 'juliett'
+        'k' = 'kilo'
+        'l' = 'lima'
+        'm' = 'mike'
+        'n' = 'november'
+        'o' = 'oscar'
+        'p' = 'papa'
+        'q' = 'quebec'
+        'r' = 'romeo'
+        's' = 'sierra'
+        't' = 'tango'
+        'u' = 'uniform'
+        'v' = 'victor'
+        'w' = 'whiskey'
+        'x' = 'xray'
+        'y' = 'yankee'
+        'z' = 'zulu'
+        '.' = '(POINT)'
+        '-' = '(DASH)'
+    }
 
-        $nato = @{
-            '0'=[PSCustomObject]@{PSOutputString='(ZERO)';Pronunciation='ZEE-RO';}
-            '1'=[PSCustomObject]@{PSOutputString='(ONE)';Pronunciation='WUN';}
-            '2'=[PSCustomObject]@{PSOutputString='(TWO)';Pronunciation='TOO';}
-            '3'=[PSCustomObject]@{PSOutputString='(THREE)';Pronunciation='TREE';}
-            '4'=[PSCustomObject]@{PSOutputString='(FOUR)';Pronunciation='FOW-ER';}
-            '5'=[PSCustomObject]@{PSOutputString='(FIVE)';Pronunciation='FIFE';}
-            '6'=[PSCustomObject]@{PSOutputString='(SIX)';Pronunciation='SIX';}
-            '7'=[PSCustomObject]@{PSOutputString='(SEVEN)';Pronunciation='SEV-EN';}
-            '8'=[PSCustomObject]@{PSOutputString='(EIGHT)';Pronunciation='AIT';}
-            '9'=[PSCustomObject]@{PSOutputString='(NINE)';Pronunciation='NIN-ER';}
-            'a'=[PSCustomObject]@{PSOutputString='alfa';Pronunciation='AL-FAH';}
-            'b'=[PSCustomObject]@{PSOutputString='bravo';Pronunciation='BRAH-VOH';}
-            'c'=[PSCustomObject]@{PSOutputString='charlie';Pronunciation='CHAR-LEE';}
-            'd'=[PSCustomObject]@{PSOutputString='delta';Pronunciation='DELL-TAH';}
-            'e'=[PSCustomObject]@{PSOutputString='echo';Pronunciation='ECK-OH';}
-            'f'=[PSCustomObject]@{PSOutputString='foxtrot';Pronunciation='FOKS-TROT';}
-            'g'=[PSCustomObject]@{PSOutputString='golf';Pronunciation='GOLF';}
-            'h'=[PSCustomObject]@{PSOutputString='hotel';Pronunciation='HOH-TEL';}
-            'i'=[PSCustomObject]@{PSOutputString='india';Pronunciation='IN-DEE-AH';}
-            'j'=[PSCustomObject]@{PSOutputString='juliett';Pronunciation='JEW-LEE-ETT';}
-            'k'=[PSCustomObject]@{PSOutputString='kilo';Pronunciation='KEY-LOH';}
-            'l'=[PSCustomObject]@{PSOutputString='lima';Pronunciation='LEE-MAH';}
-            'm'=[PSCustomObject]@{PSOutputString='mike';Pronunciation='MIKE';}
-            'n'=[PSCustomObject]@{PSOutputString='november';Pronunciation='NO-VEM-BER';}
-            'o'=[PSCustomObject]@{PSOutputString='oscar';Pronunciation='OSS-CAH';}
-            'p'=[PSCustomObject]@{PSOutputString='papa';Pronunciation='PAH-PAH';}
-            'q'=[PSCustomObject]@{PSOutputString='quebec';Pronunciation='KEH-BECK';}
-            'r'=[PSCustomObject]@{PSOutputString='romeo';Pronunciation='ROH-ME-OH';}
-            's'=[PSCustomObject]@{PSOutputString='sierra';Pronunciation='SEE-AIR-RAH';}
-            't'=[PSCustomObject]@{PSOutputString='tango';Pronunciation='TANG-GO';}
-            'u'=[PSCustomObject]@{PSOutputString='uniform';Pronunciation='YOU-NEE-FORM';}
-            'v'=[PSCustomObject]@{PSOutputString='victor';Pronunciation='VIK-TAH';}
-            'w'=[PSCustomObject]@{PSOutputString='whiskey';Pronunciation='WISS-KEY';}
-            'x'=[PSCustomObject]@{PSOutputString='xray';Pronunciation='ECKS-RAY';}
-            'y'=[PSCustomObject]@{PSOutputString='yankee';Pronunciation='YANG-KEY';}
-            'z'=[PSCustomObject]@{PSOutputString='zulu';Pronunciation='ZOO-LOO';}
-            '.'=[PSCustomObject]@{PSOutputString='(POINT)';Pronunciation='POINT';}
-            '-'=[PSCustomObject]@{PSOutputString='(DASH)';Pronunciation='DASH';}
-        }
-    } # END: BEGIN
-    Process
-    {
-        if ($pscmdlet.ShouldProcess("Target", "Operation"))
+    $chars = ($InputObject).ToCharArray()
+
+    foreach ($char in $chars) {
+        switch -Regex -CaseSensitive ($char)
         {
-            foreach ($string in $InputObject)
+            '\d'
             {
-                Write-Verbose -Message "InputObject: '$string'"
-                $sb = New-Object -TypeName 'System.Text.StringBuilder'
-                $characters = $string.ToCharArray()
-                $count = $($characters.Count)
-                for ($i = 0; $i -lt $count; $i++)
-                {
-                    $character = $characters[$i]
-                    switch -Regex -CaseSensitive ($character)
-                    {
-                        '\d'
-                        {
-                            $sb.Append($nato.Get_Item("$character").PSOutputString) | Out-Null
-                            break
-                        }
-                        '[a-z]'
-                        {
-                            $sb.Append($nato.Get_Item("$character").PSOutputString.ToLower()) | Out-Null
-                            break
-                        }
-                        '[A-Z]'
-                        {
-                            
-                            $sb.Append($nato.Get_Item("$character").PSOutputString.ToUpper()) | Out-Null
-                            break
-                        }
-                        '\.'
-                        {
-                            $sb.Append($nato.Get_Item("$character").PSOutputString) | Out-Null
-                            break
-                        }
-                        '\-'
-                        {
-                            $sb.Append($nato.Get_Item("$character").PSOutputString) | Out-Null
-                            break
-                        }
-                        Default {<# Nothing. #>}
-                    }
-                    # The string contains additional characters, append a whitespace ' ' to make the output text easier to read.
-                    if ($i -ne $($count-1))
-                    {
-                        $sb.Append(' ') | Out-Null
-                    }
-                }
-                Write-Output -InputObject $($sb.ToString())
-                Remove-Variable -Name sb,characters,count
+                $result += ($nato["$char"])
+                break
             }
+            '[a-z]'
+            {
+                $result += ($nato["$char"]).ToLower()
+                break
+            }
+            '[A-Z]'
+            {
+                $result += ($nato["$char"]).ToUpper()
+                break
+            }
+
+            Default {$result += $char}
         }
-    } # END: PROCESS
-    End
-    {
-    } # END: END
+    }
+    $result
 }
