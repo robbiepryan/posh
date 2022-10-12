09f57d868d4396d2200322af4df4f504ba8ce7d8
robbiepryan <88213739+robbiepryan@users.noreply.github.com>
2022-10-11 20:27:08 -0400

    Update Copy-MFA.ps1

--- Scripts/Copy-MFA.ps1
+++ Scripts/Copy-MFA.ps1
@@ -9,7 +9,7 @@ $codes = @{}
 
 GetCodes |
 ForEach-Object {
-    $codes.Add("$_","")
+    $codes.Add("$_", "")
 }
 
 While ($true) {
@@ -18,7 +18,17 @@ While ($true) {
     foreach ($code in $UpdatedCodes) {
         if ($code -notin $codes.Keys) {
             $code | Set-Clipboard
-            $codes.Add("$code","")
+            $codes.Add("$code", "")
+            #Add the required assembly
+            add-type -AssemblyName System.Windows.Forms
+            #Wait 100 milliseconds
+            Start-Sleep -Milliseconds 200
+            #Paste Contents
+            $clipboard = (Get-Clipboard).ToCharArray()
+            foreach ($clip in $clipboard) {
+                [System.Windows.Forms.SendKeys]::SendWait("{$clip}")
+            }
+            [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
         }
     }
 }
