8dadee1f2eecfc1ab32c575a8080f494bae20a9a
robbiepryan <88213739+robbiepryan@users.noreply.github.com>
2023-10-20 01:12:08 -0400

    working

--- Scripts/Testing/ItemPriceReport/Get-AverageItemPrice.ps1
+++ Scripts/Testing/ItemPriceReport/Get-AverageItemPrice.ps1
@@ -8,44 +8,71 @@ function Get-AverageItemPrice {
         [string]$key
     )
 
-    $enrichedData = @() # Empty array to collect enriched data
+    $global:enrichedData = @() # Empty array to collect enriched data
+    $global:products = @()
 
-    $csvContent = Import-Csv -Path $csvPath
+    $global:csvContent = Import-Csv -Path $csvPath
 
-    foreach ($row in $csvContent) {
-        #$searchTerm = (("$($row.scale) $($row.manufacturer) $($row.item)").Trim()).Replace("  ", " ")
-        $searchTerm = (($row.item).Trim()).Replace("  ", " ")
+    $itemNumber = 0
 
+    foreach ($global:row in $global:csvContent) {
+        #$global:searchTerm = (("$global:($global:row.scale) $global:($global:row.manufacturer) $global:($global:row.item)").Trim()).Replace("  ", " ")
+        $global:searchTerm = (($global:row.item).Trim()).Replace("  ", " ")
+        
+        #"category_id": "220",
         # Make a request to the RapidAPI endpoint for Ebay Average Selling Price
-        $headers = @{}
-        $headers.Add("content-type", "application/json")
-        $headers.Add("X-RapidAPI-Key", $key)
-        $headers.Add("X-RapidAPI-Host", "ebay-average-selling-price.p.rapidapi.com")
-        $body = @"
-        {
-            "keywords": "$searchTerm",
-            "excluded_keywords": "",
-            "max_search_results": "60",
-            "max_pages": "5",
-            "category_id": "220",
-            "remove_outliers": "true",
-            "site_id": "0"
-        }
-"@
-        $rapidApiResponse = Invoke-RestMethod -Uri 'https://ebay-average-selling-price.p.rapidapi.com/findCompletedItems' -Method POST -Headers $headers -ContentType 'application/json' -Body $body
-        $rapidApiResponse
+        $global:headers = @{}
+        $global:headers.Add("content-type", "application/json")
+        $global:headers.Add("X-RapidAPI-Key", $global:key)
+        $global:headers.Add("X-RapidAPI-Host", "ebay-average-selling-price.p.rapidapi.com")
 
-        try {
-            # Store the average price with the current row data
-            $row | Add-Member -Name "AveragePrice" -MemberType NoteProperty -Value $rapidApiResponse.average_price
-            $row | Add-Member -Name "MedianPrice" -MemberType NoteProperty -Value $rapidApiResponse.median_price
-            $row | Add-Member -Name "MinimumPrice" -MemberType NoteProperty -Value $rapidApiResponse.min_price
-            $row | Add-Member -Name "MaximumPrice" -MemberType NoteProperty -Value $rapidApiResponse.max_price
-            $enrichedData += $row
+        $body = @{
+            "keywords"           = $searchTerm
+            "excluded_keywords"  = ""
+            "max_search_results" = "5"
+            "max_pages"          = "5"
+            "category_id"        = "220"
+            "remove_outliers"    = "true"
+            "site_id"            = "0"
+        } | ConvertTo-Json
+
+        $response = Invoke-RestMethod -Uri 'https://ebay-average-selling-price.p.rapidapi.com/findCompletedItems' -Method POST -Headers $headers -ContentType 'application/json' -Body $body
+        $response
+
+        $itemNumber++
+        $global:listings = ($rapidApiResponse | select -expand products | Select -first 5)
+        foreach ($global:item in $listings) {
+            $global:products += [PSCustomObject]@{
+                ItemNumber    = $($itemNumber)
+                Title         = $($item.title)
+                SalePrice     = $($item.sale_price)
+                Condition     = $($item.condition)
+                BuyingFormat  = $($item.buying_format)
+                DateSold      = $($item.date_sold)
+                ImageUrl      = $($item.image_url)
+                ShippingPrice = $($item.shipping_price)
+                Link          = $($item.link)
+                ItemID        = $($item.item_id)
+            }
         }
-        catch {
-            Write-Host "Error fetching data for searchTerm: $searchTerm. Error: $_"
-        }
+
+        # $global:enrichedData += [PSCustomObject]@{
+        #     ItemNumber   = "$itemNumber"
+        #     AveragePrice = "$($global:rapidApiResponse.average_price)"
+        #     MedianPrice  = "$($global:rapidApiResponse.median_price)"
+        #     MinimumPrice = "$($global:rapidApiResponse.min_price)"
+        #     MaximumPrice = "$($global:rapidApiResponse.max_price)"
+        # }
+
+        $global:row | Add-Member -Name "AveragePrice" -MemberType NoteProperty -Value $global:rapidApiResponse.average_price -Force
+        $global:row | Add-Member -Name "MedianPrice" -MemberType NoteProperty -Value $global:rapidApiResponse.median_price -Force
+        $global:row | Add-Member -Name "MinimumPrice" -MemberType NoteProperty -Value $global:rapidApiResponse.min_price -Force
+        $global:row | Add-Member -Name "MaximumPrice" -MemberType NoteProperty -Value $global:rapidApiResponse.max_price -Force
+        $global:row | Add-Member -Name "Key" -MemberType NoteProperty -Value $itemNumber -Force
+        $global:enrichedData += $global:row
+
     }
-    $enrichedData | Export-Csv -Path $csvOutPath -NoTypeInformation
+    $global:enrichedData | Export-Csv -Path $csvOutPath -NoTypeInformation
+    $products | Export-Csv -Path "C:\temp\csv-OUT2.csv" -NoTypeInformation
 }
+
