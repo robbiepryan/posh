11df0615aac2f2b190fc72092236d628202a1395
robbiepryan <88213739+robbiepryan@users.noreply.github.com>
2023-10-19 22:18:50 -0400

    untested

--- Scripts/Testing/ItemPriceReport/Get-AverageItemPrice.ps1
+++ Scripts/Testing/ItemPriceReport/Get-AverageItemPrice.ps1
@@ -1,35 +1,45 @@
-param (
-    [Parameter(Mandatory=$true)]
-    [string]$csv
-)
+function Get-AverageItemPrice {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$csv = "C:\csv.csv",
+        [Parameter(Mandatory = $true)]
+        [string]$csvOut = "C:\csvOut.csv"
+    )
 
-# Define your API credentials
-$appId = "YOUR_APP_ID"
+    # Define your API credentials
+    $appId = "YOUR_APP_ID"
 
-# Define your endpoint - this example uses the Finding API's 'findCompletedItems' call
-$endpointUrl = "https://svcs.ebay.com/services/search/FindingService/v1"
+    # Define your endpoint - this example uses the Finding API's 'findCompletedItems' call
+    $endpointUrl = "https://svcs.ebay.com/services/search/FindingService/v1"
 
-# Define your request parameters
-$params = @{
-    "OPERATION-NAME" = "findCompletedItems"
-    "SERVICE-VERSION" = "1.13.0"
-    "SECURITY-APPNAME" = $appId
-    "RESPONSE-DATA-FORMAT" = "XML" # or "JSON" if you prefer
-    "REST-PAYLOAD" = $true
-    "keywords" = "YOUR_SEARCH_KEYWORDS" # replace with specific items you're searching for
-    # Add other parameters as necessary
-}
+    foreach ($row in $csv) {
+        $searchTerm = (("$($row.scale) $($row.manufacturer) $($row.item)").Trim()).Replace("  ", " ")
+        # Define your request parameters
+        $params = @{
+            "OPERATION-NAME"       = "findCompletedItems"
+            "SERVICE-VERSION"      = "1.13.0"
+            "SECURITY-APPNAME"     = $appId
+            "RESPONSE-DATA-FORMAT" = "XML" # or "JSON" if you prefer
+            "REST-PAYLOAD"         = $true
+            "keywords"             = "$searchTerm" # replace with specific items you're searching for
+            # Add other parameters as necessary
+        }
 
-# Call the API
-$response = Invoke-RestMethod -Uri $endpointUrl -Method Get -Headers $params
+        # Call the API
+        $response = Invoke-RestMethod -Uri $endpointUrl -Method Get -Headers $params
 
-# Parse and process the response
-# This step will vary based on your needs and the structure of the API response
-$items = $response.findCompletedItemsResponse.searchResult.item
+        # Parse and process the response
+        # This step will vary based on your needs and the structure of the API response
+        $items = $response.findCompletedItemsResponse.searchResult.item
 
-# Example: Print the title and price of each item
-$items | ForEach-Object {
-    $title = $_.title
-    $price = $_.sellingStatus.currentPrice."#text"
-    Write-Host "Title: $title, Price: $price"
+        # Example: Print the title and price of each item
+        $items | ForEach-Object {
+            $title = $_.title
+            $price = $_.sellingStatus.currentPrice."#text"
+            Write-Host "Title: $title, Price: $price"
+            $csv | Add-Member -Name "Title" -MemberType NoteProperty -Value $title
+            $csv | Add-Member -Name "Price" -MemberType NoteProperty -Value $price
+        }
+    }
+    $csv | Export-Csv -Path $csvOut -NoTypeInformation
 }
